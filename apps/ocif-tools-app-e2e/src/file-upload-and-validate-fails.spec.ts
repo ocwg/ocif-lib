898af752dfc325d0599675af9333d54eb3813d0e
Maikel van de Lisdonk <maikel@devhelpr.com>
2025-06-29 10:51:39 +0200

    playwright fix: update file upload paths to use path.resolve for better compatibility

--- apps/ocif-tools-app-e2e/src/file-upload-and-validate-fails.spec.ts
+++ apps/ocif-tools-app-e2e/src/file-upload-and-validate-fails.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
+import path = require('path');
 
 test('file upload and validation fails', async ({ page }) => {
   await page.goto('/');
 
   expect(await page.locator('h1').innerText()).toContain('OCIF JSON Validator');
-  await page
-    .locator('input[type="file"]')
-    .setInputFiles('test-files/bad.ocif.json');
+  const filePath = path.resolve(__dirname, '../test-files/bad.ocif.json');
+  await page.locator('input[type="file"]').setInputFiles(filePath);
   expect(await page.locator('h3').innerText()).toContain('Validation Failed');
 });
--- apps/ocif-tools-app-e2e/src/file-upload-and-validate.spec.ts
+++ apps/ocif-tools-app-e2e/src/file-upload-and-validate.spec.ts
@@ -1,12 +1,15 @@
 import { test, expect } from '@playwright/test';
+import path = require('path');
 
 test('file upload and validation', async ({ page }) => {
   await page.goto('/');
 
   expect(await page.locator('h1').innerText()).toContain('OCIF JSON Validator');
-  await page
-    .locator('input[type="file"]')
-    .setInputFiles('test-files/hello-world.ocif.json');
+  const filePath = path.resolve(
+    __dirname,
+    '../test-files/hello-world.ocif.json'
+  );
+  await page.locator('input[type="file"]').setInputFiles(filePath);
   expect(await page.locator('h3').innerText()).toContain(
     'Validation Successful!'
   );
